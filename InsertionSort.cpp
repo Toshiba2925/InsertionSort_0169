2fa34805394ae4497479a02d1eae768db881cff0
Toshiba2925 <aqifashiba25@gmail.com>
2025-03-20 11:55:17 +0700

    Menambahkan Insertion Sort

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -24,5 +24,19 @@ void input(){
         cout << "Data ke-" << (i + 1) << ": ";
         cin >> arr[i];
     }
+}
 
+void insertionSort(){
+    int temp;
+    int j, i;
+
+    for (i = 1; i <= n - 1; i++){
+        temp = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > temp){
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
 }
