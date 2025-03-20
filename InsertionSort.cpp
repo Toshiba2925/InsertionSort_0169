45821530535e9a1ca1444ade27b1b8645309fdbb
Toshiba2925 <aqifashiba25@gmail.com>
2025-03-20 11:49:50 +0700

    Menambahkan pengulangan for

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -20,4 +20,9 @@ void input(){
     cout << "Masukkan element array" << endl;
     cout << "======================" << endl;
 
+    for (int i = 0; i < n; i++){
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+
 }
